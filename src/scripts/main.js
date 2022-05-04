09f51997eb4dc0af6271d39634028e39a6add94a
Ryley Hynes <ryleyhynes@gmail.com>
2022-05-04 14:06:15 -0500

    created and fired pottery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 // **NOT MY NOTE** Imports go first
 
 //when importing note that from is the same as = in this function
-import {makePottery} from "./PotteryWheel"
-
+import {makePottery} from "./PotteryWheel.js"
+import {firePottery} from "./Kiln.js"
 // **NOT MY NOTE** Make 5 pieces of pottery at the wheel
 
 //here we are invoking the makePottery function and assigning its return value to a constant variable
@@ -10,12 +10,21 @@ const vase = makePottery("cylindrical",10,7)
 const bowl = makePottery("round",4,3)
 const plate = makePottery("concave",3,2)
 const cup = makePottery("cup",2,4)
-const mug = makePottery("mug",3,)
+const mug = makePottery("mug",3,6)
 
+// console.log(vase,bowl,plate,cup,mug)
 
 
 
 //**NOT MY NOTE** Fire each piece of pottery in the kiln
+const firedVase = firePottery(vase);
+const firedBowl = firePottery(bowl);
+const firedPlate = firePottery(plate);
+const firedCup = firePottery(cup);
+const firedMug = firePottery(mug);
+
+console.log(firedVase,firedBowl,firedPlate,firedCup,firedMug)
+
 
 
 // **NOT MY NOTE** Determine which ones should be sold, and their price
